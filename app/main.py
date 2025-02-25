a4c80acb581934afcb6f1691c4d2204075c31245
Obcodelab <obcodelab1@gmail.com>
2025-02-25 18:09:51 +0100

    allowed more methods

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
